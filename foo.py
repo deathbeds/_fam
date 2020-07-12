71b363bf6120da72e841e3ecffd597fd17866c3b
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-07-11 21:27:00 -0400

    Create foo.py

---
+++ foo.py
@@ -0,0 +1,5 @@
+### start blockly
+for count in range(10):
+    pass
+### end blockly
+### workspace: <xml xmlns="https://developers.google.com/blockly/xml"><block type="controls_repeat_ext" id="%lp}9GcpdIX{7RcnQnt?" x="97" y="80"><value name="TIMES"><shadow type="math_number" id="E^rpI?Hvg}((aN~4;(7k"><field name="NUM">10</field></shadow></value></block></xml>
